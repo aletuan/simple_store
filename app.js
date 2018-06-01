bcfa4642b821e49c94d1dd7f1e77f4e33fb4f89e
Tuan Anh Le <anhlt1983@gmail.com>
2018-06-01 20:03:40 +0700

    initial commit

---
+++ app.js
@@ -0,0 +1,18 @@
+function reducer(state, action) {
+    if (action.type === 'INCREMENT') {
+        return state + 1;
+    } else {
+        return state;
+    }
+}
+
+const incrementAction = { type: 'INCREMENT' };
+
+console.log(reducer(0, incrementAction));
+console.log(reducer(1, incrementAction));
+console.log(reducer(5, incrementAction));
+
+const unknownAction = { type: 'UNKNOWN' };
+
+console.log(reducer(5, unknownAction));
+console.log(reducer(8, unknownAction));
