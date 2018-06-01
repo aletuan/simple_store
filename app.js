8c3f0543c05d252d8e71d0107045cd9bb7ea435a
Tuan Anh Le <anhlt1983@gmail.com>
2018-06-01 20:07:54 +0700

    adding param of action

--- app.js
+++ app.js
@@ -1,18 +1,28 @@
 function reducer(state, action) {
     if (action.type === 'INCREMENT') {
-        return state + 1;
+        return state + action.amount;
     } else if (action.type === 'DECREMENAT') {
-        return state - 1;
+        return state - action.amount;
     } else {
         return state;
     }
 }
 
-const incrementAction = { type: 'INCREMENT' };
+const incrementAction = { 
+    type: 'INCREMENT',
+    amount: 5, 
+};
 
 console.log(reducer(0, incrementAction));
 console.log(reducer(1, incrementAction));
 console.log(reducer(5, incrementAction));
+
+const decrementAction = {
+    type: 'DECREMENT',
+    amount: 11,
+};
+
+console.log(reducer(100, decrementAction));
 
 const unknownAction = { type: 'UNKNOWN' };
 
